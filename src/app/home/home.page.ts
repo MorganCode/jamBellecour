ec6b3b2ac56761261a7fdaab0b7ed8c5ba76c6ca
mperhirin <mperhirin.externe@m6.fr>
2020-02-01 12:30:13 +0100

    add background

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,51 @@
 import { Component } from "@angular/core";
+import { MenuController } from "@ionic/angular";
+
+declare let Phaser;
+
+let that;
+let game;
+// Background
+let background;
+// Device
+let heightDevice;
+let widthDevice;
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
   styleUrls: ["home.page.scss"]
 })
 export class HomePage {
-  constructor() {}
+  constructor(private menuCtrl: MenuController) {
+    game = new Phaser.Game(
+      window.innerWidth,
+      window.innerHeight,
+      Phaser.AUTO,
+      "space-invaders",
+      {
+        preload: this.preload,
+        create: this.create
+        // update: this.update,
+        // render: this.render
+      }
+    );
 
-  show() {
-    console.log("hello");
+    that = Object.create(this.constructor.prototype);
+  }
+
+  // Preload this game
+  preload() {
+    heightDevice = window.innerHeight;
+    widthDevice = window.innerWidth;
+    game.load.image("background", "assets/phaser/Arena_test.png");
+  }
+
+  create() {
+    console.log(heightDevice);
+    console.log(widthDevice);
+    // Set background
+    background = game.add.image(10, 0, "background");
+    background.scale.set(0.35);
+    // back.smoothed = false;
   }
 }
